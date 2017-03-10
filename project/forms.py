f5d5b7423bbacebf2c5d6de33f19539199f4ac99
tian-gao <tian.gao@columbia.edu>
2017-03-09 22:27:37 -0500

    final

--- project/forms.py
+++ project/forms.py
@@ -4,5 +4,6 @@ from wtforms import StringField, TextAreaField, SubmitField, validators
 class ContactForm(Form):
     name = StringField("Name", [validators.DataRequired()], render_kw={"placeholder": "Name"})
     email = StringField("Email", [validators.DataRequired(), validators.Email('your@email.com')], render_kw={"placeholder": "E-Mail"})
-    message = TextAreaField("Message", [validators.DataRequired()], render_kw={"placeholder": "Message", "rows": 4})
+    message = TextAreaField("Message", [validators.DataRequired()], 
+    	render_kw={"placeholder": "Message (banned for Flask frozen pages)", "rows": 4})
     submit = SubmitField("Send Message")
--- project/views.py
+++ project/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask import request
 
 from app import app, pages
@@ -10,64 +10,31 @@ def home():
     for page in pages:
         sec = page.meta['section']
         d[sec] = d.get(sec, page.meta['content'])
-    form = ContactForm()
-    err_string = []
-    show_success = False
-    
+    form = ContactForm(request.form)
+
     if request.method == 'POST':
         if form.validate_on_submit() == False:
-            print 'Please fill in all fields'
             for field, errors in form.errors.items():
                 for error in errors:
-                    err_string += ["Error in the %s field - %s" % (getattr(form, field).label.text, error)]
-            print err_string
-            show_success = False
-            return render_template('index.html', 
-                education = d['education'],
-                professional = d['professional'],
-                extracurricular = d['extracurricular'],
-                publications = d['publications'],
-                honors = d['honors'],
-                skills = d['skills'],
-                form = form,
-                err_string = err_string,
-                show_success = show_success)
+                    if error == "your@email.com":
+                        flash("Wrong format of email address!", 'error')
+                    else:
+                        flash("%s cannot be empty!" % getattr(form, field).label.text, 'error')
+            return redirect(url_for('home', _anchor='contact'))
 
         else:
-            # msg = Message("Message from your visitor" + form.name.data,
-            #               sender='YourUser@NameHere',
-            #               recipients=['yourRecieve@mail.com', 'someOther@mail.com'])
-            # msg.body = """
-            # From: %s <%s>,
-            # %s
-            # """ % (form.name.data, form.email.data, form.message.data)
-            # mail.send(msg)
-            # return "Successfully  sent message!"
-            show_success = True
-            print '\nSuccessfully  sent message!\n'
+            flash('Successfully send message!', 'success')
+            print '\nSuccessfully send message!\n'
             print request.form['name']
             print request.form['email']
             print request.form['message']
-            return render_template('index.html', 
-                education = d['education'],
-                professional = d['professional'],
-                extracurricular = d['extracurricular'],
-                publications = d['publications'],
-                honors = d['honors'],
-                skills = d['skills'],
-                form = form,
-                err_string = err_string,
-                show_success = show_success)
-            # return redirect(url_for('home'), )
+            return redirect(url_for('home', _anchor='contact'))
 
-    else:
-        return render_template('index.html', 
-            education = d['education'],
-            professional = d['professional'],
-            extracurricular = d['extracurricular'],
-            publications = d['publications'],
-            honors = d['honors'],
-            skills = d['skills'],
-            form = form,
-            err_string = err_string,
-            show_success = show_success)
+    return render_template('index.html', 
+        education = d['education'],
+        professional = d['professional'],
+        extracurricular = d['extracurricular'],
+        publications = d['publications'],
+        honors = d['honors'],
+        skills = d['skills'],
+        form = form)
